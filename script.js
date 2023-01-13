8ef050883bd33740fe88f0c758dfeb61cbed8aa6
Ajay Menon <ajaymenon0@gmail.com>
2023-01-13 18:47:26 +0530

    feat: volume button functional & cutoff button

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const headTitle = document.title;
+const DEFAULT_VOLUME = 30;
+const DEFAULT_CUTOFF = 300;
+let vol = DEFAULT_VOLUME;
+let cutoff = DEFAULT_CUTOFF;
+
 class Noise {
   constructor(settings) {
     this.audioContext = new (window.AudioContext ||
@@ -6,7 +11,7 @@ class Noise {
     this.noiseNode = this.audioContext.createBufferSource();
     this.gainNode = this.audioContext.createGain();
     this.gainNode.gain.value = 0;
-    this.volume = settings.volume || 0.5; // init volume
+    this.volume = settings.volume ?? 0.5; // init volume
 
     this.filterNode = this.audioContext.createBiquadFilter();
     this.filterNode.type = 'lowpass';
@@ -52,7 +57,7 @@ playpauseButton.addEventListener('click', () => {
   if(buttonIcon.classList.contains('ic-play')) {
     buttonIcon.classList.remove('ic-play');
     buttonIcon.classList.add('ic-pause');
-    newNoise = new Noise({ volume: 0.5, cutoff: 300 });
+    newNoise = new Noise({ volume: vol/100, cutoff: 300 });
     newNoise.play();
   } else {
     buttonIcon.classList.remove('ic-pause');
@@ -60,3 +65,94 @@ playpauseButton.addEventListener('click', () => {
     newNoise.stop();
   }
 });
+
+// Credits: https://www.freecodecamp.org/news/javascript-debounce-example/
+function debounce(func, timeout = 300){
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => { func.apply(this, args); }, timeout);
+  };
+}
+
+function initNoiseMaker(){
+  const settings = {
+    volume: vol / 100,
+    cutoff: 300
+  }
+  newNoise = new Noise(settings);
+  newNoise.play();
+}
+
+let volDial = document.getElementById('vol');
+let speaker = document.getElementById('speaker');
+let dial = document.getElementById('dial');
+let body = document.body;
+
+const volClick = (event) => {
+  // localStorage.setItem('volume', e);
+  var startX = event.clientX;
+  var startY = event.clientY;
+  var startVol = vol;
+
+  function drag(e){
+    body.style.cursor = 'grabbing';
+    volDrag(e.clientX,e.clientY,startX,startY,startVol);
+  }
+  // const drag = debounce((e) => {volDrag(e.clientX,e.clientY,startX,startY,startVol);});
+
+  window.addEventListener('mousemove', drag);
+  window.addEventListener('mouseup', function undrag(){
+    window.removeEventListener('mousemove', drag);
+    body.style.cursor = 'unset';
+    window.removeEventListener('mouseup', undrag);
+  });
+  
+};
+
+// let vol = 0;
+
+// function volClick(event){
+
+//   var startX = event.clientX;
+//   var startY = event.clientY;
+//   var startVol = vol;
+
+//   function drag(e){
+//     volDrag(e.clientX,e.clientY,startX,startY,startVol);
+//   }
+
+//   window.addEventListener('mousemove', drag);
+//   window.addEventListener('mouseup', function undrag(){
+//     window.removeEventListener('mousemove', drag);
+//     window.removeEventListener('mouseup', undrag);
+//   });
+// }
+
+const checkStartNoise = debounce(() => {
+  if(newNoise) {
+    newNoise.stop();
+    initNoiseMaker();
+  }
+});
+
+const updateVol = () => {
+  dial.style.transform = `rotate(${vol * 1.8}deg)`;
+  if(vol < 1) {
+    speaker.style.opacity = 0.4;
+    dial.style.opacity = 0.4;
+  } else {
+    speaker.style.opacity = 1;
+    dial.style.opacity = 1;
+  }
+  checkStartNoise();
+}
+
+function volDrag(x,y, startX, startY, startVol){
+  var moveDist = (x - startX + startY - y) / window.innerHeight;
+  var volChange = (moveDist / 0.25) * 100;
+  vol = Math.max(Math.min((startVol + volChange), 100),0);
+  updateVol();
+}
+
+updateVol();
